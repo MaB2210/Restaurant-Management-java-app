690e11cecc24bb1809bd61f61f563f62872932d7
MaB2210 <mannbhatt.00@gmail.com>
2024-08-05 09:04:16 -0400

    Cook also done

--- src/hotel/entity/Cook.java
+++ src/hotel/entity/Cook.java
@@ -1,6 +1,11 @@
 package hotel.entity;
 
-public class Cook {
+public class Cook extends Employee {
+    public Cook(String name, String cuisine, boolean foodCertification){
+        super(name);
+        this.cuisine=cuisine;
+        this.foodCertification=foodCertification;
+    }
 
     private String cuisine;
     private boolean foodCertification;
@@ -19,5 +24,15 @@ public class Cook {
 
     public void setFoodCertification(boolean foodCertification) {
         this.foodCertification = foodCertification;
+    }
+
+    @Override
+    public void performDuty(){
+        if(foodCertification == true){
+            System.out.println("Chef " + getName() + "is expert in " + getCuisine() + " Cuisine and is Food Safety Certified.");
+        }
+        else {
+            System.out.println("Chef " + getName() + "is expert in " + getCuisine() + " Cuisine But is not Food Safety Certified.");
+        }
     }
 }
